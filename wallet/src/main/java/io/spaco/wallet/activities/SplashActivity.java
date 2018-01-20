87d93bcdcb39c4dd61469ce78150b6114048c2e3
zhjy <691136156@qq.com>
2018-01-20 23:02:12 +0800

    提交了：增加了一些比如开屏页面，一直一些简单的页面

--- wallet/src/main/java/io/spaco/wallet/activities/SplashActivity.java
+++ wallet/src/main/java/io/spaco/wallet/activities/SplashActivity.java
@@ -1,5 +1,11 @@
 package io.spaco.wallet.activities;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+
+import io.spaco.wallet.R;
 import io.spaco.wallet.base.BaseActivity;
 
 /**
@@ -7,4 +13,27 @@ import io.spaco.wallet.base.BaseActivity;
  */
 
 public class SplashActivity extends BaseActivity {
+    @Override
+    protected int getContentViewId() {
+        return R.layout.activity_splash;
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, PinSetActivity.class));
+            }
+        },3000);
+    }
+
+    /**
+     * 如果pin没有设定的话，跳到pin设置页面
+     * 如果没有创建钱包，则跳转到pin输入页面
+     */
+    private void launchToNextAcitivity(){
+
+    }
 }
---
+++ wallet/src/main/java/io/spaco/wallet/utils/SpacoWalletUtils.java
@@ -0,0 +1,14 @@
+package io.spaco.wallet.utils;
+
+/**
+ * Created by zjy on 2018/1/20.
+ */
+
+/**
+ * 目前没有啥需要的类，所以以后复杂了再拆分
+ */
+public class SpacoWalletUtils {
+    public static boolean isPinSet(){
+        return true;
+    }
+}
