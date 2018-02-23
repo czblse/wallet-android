5f87627ec1aa67b8170c017503433ec1f94cb722
kimi <24750@163.com>
2018-02-23 20:02:52 +0800

    钱包观察者编码实现

---
+++ wallet/src/main/java/io/spaco/wallet/push/WalletListener.java
@@ -0,0 +1,35 @@
+package io.spaco.wallet.push;
+
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ * Created by kimi on 2018/2/22.
+ */
+
+public abstract class WalletListener implements Observer {
+
+    @Override
+    public void update(Observable o, Object arg) {
+        try {
+            int value = (int) arg;
+            switch (value){
+                case WalletPush.WALLET_UPDATE:
+                    walletUpdate();
+                    break;
+                case WalletPush.TRANSACTION_UPDATE:
+                    transactionUpdate();
+                    break;
+                case WalletPush.BALANCE_UPDATE:
+                    balanceUpdate();
+                    break;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    protected abstract void walletUpdate();
+    protected abstract void transactionUpdate();
+    protected abstract void balanceUpdate();
+}
