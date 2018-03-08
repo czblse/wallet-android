b436e16128729954aa3031bbcd52080278a9c682
jinyang.zheng <jinyang.zheng@nio.com>
2018-03-08 19:06:27 +0800

    pin页面按返回回到桌面

--- wallet/src/main/java/io/spaco/wallet/activities/PinSetActivity.java
+++ wallet/src/main/java/io/spaco/wallet/activities/PinSetActivity.java
@@ -182,6 +182,9 @@ public class PinSetActivity extends BaseActivity implements PinSetListener {
      */
     @Override
     public void onBackPressed() {
-        finish();
+        Intent home = new Intent(Intent.ACTION_MAIN);
+        home.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        home.addCategory(Intent.CATEGORY_HOME);
+        startActivity(home);
     }
 }
