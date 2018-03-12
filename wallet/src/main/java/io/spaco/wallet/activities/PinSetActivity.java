28ffdc762d166c7ef93c68e754c71b37e3f95ce6
jinyang.zheng <jinyang.zheng@nio.com>
2018-03-12 15:27:34 +0800

    修改了注册的url

--- wallet/src/main/java/io/spaco/wallet/activities/PinSetActivity.java
+++ wallet/src/main/java/io/spaco/wallet/activities/PinSetActivity.java
@@ -201,8 +201,8 @@ public class PinSetActivity extends BaseActivity implements PinSetListener {
             String walletDir = getApplicationContext().getFilesDir().toString() + "/spo";
             try {
                 Mobile.init(walletDir, SpacoWalletUtils.getPin16());
-                Mobile.registerNewCoin("spocoin", "47.75.36.182:8620");
-                Mobile.registerNewCoin("skycoin", "47.75.36.182:6420");
+                Mobile.registerNewCoin("spocoin", "i.spo.network:8620");
+                Mobile.registerNewCoin("skycoin", "i.spo.network:6420");
             } catch (Exception e) {
                 e.printStackTrace();
             }
