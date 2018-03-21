b64af4001fe7162a410a41769b51b561b8d13084
luhang <15333805663@sina.cn>
2018-03-21 17:31:52 +0800

    主页调整

--- wallet/src/main/java/io/spaco/wallet/activities/Main/MeFragment.kt
+++ wallet/src/main/java/io/spaco/wallet/activities/Main/MeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import io.spaco.wallet.R
 import io.spaco.wallet.base.BaseFragment
+import kotlinx.android.synthetic.main.fragment_me.*
 
 /**
  * @author: lh on 2018/3/21 11:14.
@@ -11,14 +12,44 @@ import io.spaco.wallet.base.BaseFragment
  * Description: 主页 -我
  */
 
-class MeFragment : BaseFragment(), MeListentr {
+class MeFragment : BaseFragment(), View.OnClickListener, MeListentr {
 
 
     override fun attachLayoutRes(): Int {
         return R.layout.fragment_me
     }
 
+    override fun onClick(p0: View?) {
+        when (id) {
+            R.id.tv_managerwallet -> {
+                managerWallet()
+            }
+            R.id.lin_jyjl -> {
+                transactionRecord()
+            }
+            R.id.lin_xxtz -> {
+                alerts()
+            }
+            R.id.lin_xtsz -> {
+                systemSettings()
+            }
+            R.id.lin_bzzx -> {
+                helpCenter()
+            }
+            R.id.lin_gywm -> {
+                aboutUs()
+            }
+        }
+    }
+
+
     override fun initViews(rootView: View?) {
+        lin_jyjl.setOnClickListener(this)
+        lin_gywm.setOnClickListener(this)
+        lin_xxtz.setOnClickListener(this)
+        lin_xtsz.setOnClickListener(this)
+        lin_bzzx.setOnClickListener(this)
+        tv_managerwallet.setOnClickListener(this)
     }
 
     override fun initData() {
--- wallet/src/main/java/io/spaco/wallet/activities/Main/WalletFragment.kt
+++ wallet/src/main/java/io/spaco/wallet/activities/Main/WalletFragment.kt
@@ -179,27 +179,6 @@ class WalletFragment : BaseFragment(), WalletListener {
         //loadUpgradeInfo();
     }
 
-    /**
-     * 获取升级信息
-     */
-    private fun loadUpgradeInfo() {
-        val upgradeInfo = Beta.getUpgradeInfo() ?: return
-        val info = StringBuilder()
-        info.append("id: ").append(upgradeInfo.id).append("\n")
-        info.append("标题: ").append(upgradeInfo.title).append("\n")
-        info.append("升级说明: ").append(upgradeInfo.newFeature).append("\n")
-        info.append("versionCode: ").append(upgradeInfo.versionCode).append("\n")
-        info.append("versionName: ").append(upgradeInfo.versionName).append("\n")
-        info.append("发布时间: ").append(upgradeInfo.publishTime).append("\n")
-        info.append("安装包Md5: ").append(upgradeInfo.apkMd5).append("\n")
-        info.append("安装包下载地址: ").append(upgradeInfo.apkUrl).append("\n")
-        info.append("安装包大小: ").append(upgradeInfo.fileSize).append("\n")
-        info.append("弹窗间隔（ms）: ").append(upgradeInfo.popInterval).append("\n")
-        info.append("弹窗次数: ").append(upgradeInfo.popTimes).append("\n")
-        info.append("发布类型（0:测试 1:正式）: ").append(upgradeInfo.publishType).append("\n")
-        info.append("弹窗类型（1:建议 2:强制 3:手工）: ").append(upgradeInfo.upgradeType)
-        ToastUtils.show(info.toString())
-    }
 
     override fun onItemClick(position: Int, bean: Wallet) {
         val intent = Intent(activity, WalletDetailsActivity::class.java)
