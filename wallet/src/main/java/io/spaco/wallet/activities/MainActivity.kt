c49ff77c7d5f1c6f995a31cd32cbc26b9c7b6d81
luhang <15333805663@sina.cn>
2018-03-21 11:07:33 +0800

    主页调整

---
+++ wallet/src/main/java/io/spaco/wallet/activities/MainActivity.kt
@@ -0,0 +1,75 @@
+package io.spaco.wallet.activities
+
+import android.content.Intent
+import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
+import android.view.View
+import io.spaco.wallet.R
+import io.spaco.wallet.activities.Main.WalletFragment
+import io.spaco.wallet.activities.Transaction.TransactionFragment
+import io.spaco.wallet.base.BaseActivity
+import io.spaco.wallet.common.Constant
+
+/**
+ * 主界面，一级界面
+ */
+class MainActivity : BaseActivity() {
+
+    internal lateinit var mainWalletFragment: WalletFragment
+    internal lateinit var mainTransactionFragment: TransactionFragment
+    private var createWallet: View? = null
+    private var importWallet: View? = null
+    override fun attachLayoutRes(): Int {
+        return R.layout.activity_main
+    }
+
+    override fun initViews() {
+
+        mainWalletFragment = WalletFragment.newInstance(null)
+        mainTransactionFragment = TransactionFragment.newInstance(null)
+        createWallet = findViewById(R.id.new_wallet)
+        importWallet = findViewById(R.id.import_wallet)
+        createWallet!!.setOnClickListener { onCreateWallet() }
+        importWallet!!.setOnClickListener { onImportWallet() }
+        switchFragment(mainWalletFragment)
+    }
+
+    /**
+     * 切换碎片视图
+     * @param fragment
+     */
+    private fun switchFragment(fragment: Fragment) {
+        val tag = fragment.javaClass.simpleName
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        val instance = supportFragmentManager.findFragmentByTag(tag)
+        if (instance == null) {
+            fragmentTransaction.add(R.id.main_container, fragment, tag)
+        }
+        val fragments = supportFragmentManager.fragments
+        if (fragments != null) {
+            for (item in fragments) {
+                if (item == fragment) {
+                    fragmentTransaction.show(item)
+                } else {
+                    fragmentTransaction.hide(item)
+                }
+            }
+        }
+        fragmentTransaction.commit()
+    }
+
+    override fun initData() {
+
+    }
+
+    fun onCreateWallet() {
+        val intent = Intent(this, WalletCreatActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun onImportWallet() {
+        val intent = Intent(this, WalletCreatActivity::class.java)
+        intent.putExtra(Constant.KEY_PAGE, 1)
+        startActivity(intent)
+    }
+}
